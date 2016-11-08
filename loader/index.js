98c6d125dbddf461c9e346a7f745464703951560
Simon Relet <simon.relet@gmail.com>
2016-11-08 20:09:35 +0100

    feat(validation): add ast validation

--- loader/index.js
+++ loader/index.js
@@ -2,8 +2,25 @@
 
 const htmlToReact = require('../lib');
 
+function formatMessage(obj) {
+  if (typeof obj.line !== 'undefined') {
+    return `  Line ${obj.line}:  ${obj.message}`;
+  }
+  return `  ${obj.message}`;
+}
+
 module.exports = function(content) {
   this.cacheable();
+
   const res = htmlToReact({ html: content });
+
+  res.errors.forEach(err => {
+    this.emitError(formatMessage(err));
+  });
+
+  res.warnings.forEach(warn => {
+    this.emitWarning(formatMessage(warn));
+  });
+
   return res.reactStr;
 };
